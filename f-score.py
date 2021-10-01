badaf3d98f6577a8bffab3aa35428774920c3e58
Jack McKechnie <2319658M@student.gla.ac.uk>
2021-10-01 14:52:18 +0100

    Creation of a function to calculate the Heylighen & Dewaele F-Score of given input text

---
+++ f-score.py
@@ -0,0 +1,29 @@
+import nltk
+
+
+def f_score(sentence):
+
+    tokens = nltk.word_tokenize(sentence)
+    pos = nltk.pos_tag(tokens)
+
+    f_count = 0
+    c_count = 0
+
+    # Nouns, adjectives, prepositions and articles. Does not yet include articles
+    f_types = ["NN", "NNS", "JJ", "JJR", "JJS", "IN"]
+    #Pronouns, verbs, adverbs and interjections
+    c_types = ["PRP", "PRP$", "WP", "WP$", "VB", "VBD",
+               "VBG", "VBN", "VBP", "VBZ", "RB", "RBR", "RBS", "UH"]
+
+    for point in pos:
+        if point[1] in f_types:
+            f_count += 1
+        if point[1] in c_types:
+            c_count += 1
+
+    f_score = (f_count - c_count + 100) / 2
+    
+    return f_score
+
+
+
